6b3d8a41df90fbdfe9faf03171eb4c591ade61be
Vladyslav Krylasov <vladyslav.krylasov@gmail.com>
2019-12-22 12:32:18 +0200

    ✨ Add a dummy app.

---
+++ app/models.py
@@ -0,0 +1,9 @@
+def dummy_model():
+    """It is a dummy model.
+
+    >>> dummy_model()
+    False
+
+    """
+
+    return True
---
+++ app/views.py
@@ -0,0 +1,21 @@
+def view():
+    """A dummy view.
+
+    >>> view(1)
+    2
+
+    """
+
+    ...
+
+
+def util(x, y):
+    """A dummy util.
+
+
+    >>> util(1, 2)
+    3
+
+    """
+
+    return x + y
